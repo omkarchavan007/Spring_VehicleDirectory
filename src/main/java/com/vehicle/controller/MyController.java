7dce620d43f0e97db979a662d2fee6756462541f
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-06 23:27:05 +0530

    Added  Configuration class to get vehicle by ID

--- src/main/java/com/vehicle/controller/MyController.java
+++ src/main/java/com/vehicle/controller/MyController.java
@@ -1,14 +1,12 @@
 package com.vehicle.controller;
 
 import com.vehicle.entity.Vehicles;
+import com.vehicle.repository.VehicleRepo;
 import com.vehicle.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,17 +19,22 @@ public class MyController {
 
     //Add Data into DB
     @PostMapping("/addvehicle")
-    public ResponseEntity<String> addVehicle(@RequestBody Vehicles vehicles)
-    {
+    public ResponseEntity<String> addVehicle(@RequestBody Vehicles vehicles) {
         vehicleService.saveVehicles(vehicles);
         return new ResponseEntity<>("Vehicle data Saved", HttpStatus.OK);
     }
 
     // Add Multiple vehicles at once
     @PostMapping("/addMultipleVehicles")
-    public ResponseEntity<List<Vehicles>> addMultipleVehicles(@RequestBody List<Vehicles> vehicles)
-    {
-     List<Vehicles> vehicleslist = vehicleService.saveAllVehicles(vehicles);
+    public ResponseEntity<List<Vehicles>> addMultipleVehicles(@RequestBody List<Vehicles> vehicles) {
+        List<Vehicles> vehicleslist = vehicleService.saveAllVehicles(vehicles);
         return new ResponseEntity<>(vehicleslist, HttpStatus.OK);
+    }
+
+    @GetMapping("/getVehicleById/{id}")
+    public ResponseEntity <Vehicles> getVehicleByID(@PathVariable("id") int id)
+    {
+    Vehicles vehicles =vehicleService.getByID(id);
+    return new ResponseEntity<>(vehicles, HttpStatus.OK);
     }
 }
