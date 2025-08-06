850c8a62f46b490ec01e394350b55e70a944b198
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-06 22:03:33 +0530

    Created Vehicle Class and added Required Entities

--- src/main/java/com/vehicle/entity/Vehicles.java
+++ src/main/java/com/vehicle/entity/Vehicles.java
@@ -1,4 +1,70 @@
 package com.vehicle.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Vehicles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String vehicleColor;
+    private String VehicleNo;
+    private int vehicleYear;
+    private String vehicleName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVehicleColor() {
+        return vehicleColor;
+    }
+
+    public void setVehicleColor(String vehicleColor) {
+        this.vehicleColor = vehicleColor;
+    }
+
+    public String getVehicleNo() {
+        return VehicleNo;
+    }
+
+    public void setVehicleNo(String vehicleNo) {
+        VehicleNo = vehicleNo;
+    }
+
+    public int getVehicleYear() {
+        return vehicleYear;
+    }
+
+    public void setVehicleYear(int vehicleYear) {
+        this.vehicleYear = vehicleYear;
+    }
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicles{" +
+                "id=" + id +
+                ", vehicleColor='" + vehicleColor + '\'' +
+                ", VehicleNo='" + VehicleNo + '\'' +
+                ", vehicleYear=" + vehicleYear +
+                ", vehicleName='" + vehicleName + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/vehicle/repository/VehicleRepo.java
+++ src/main/java/com/vehicle/repository/VehicleRepo.java
@@ -1,4 +1,4 @@
 package com.vehicle.repository;
 
-public class StudentRepo {
+public class VehicleRepo {
 }
